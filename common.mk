7ea38e13a50a257e43697577fff98f0d81e2c355
ComicoX <comico_work@outlook.com>
2021-08-07 15:44:30 +0300

    Build QC Fm

--- common.mk
+++ common.mk
@@ -112,6 +112,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vndk_package
 
+# FM
+PRODUCT_PACKAGES += \
+    FM2 \
+    libqcomfm_jni \
+    qcom.fmradio
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
